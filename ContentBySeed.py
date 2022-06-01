4c7bbfa16d0dc0eac15b06e5dad4153f57b10ce2
Derek Gooding <djgooding@live.com>
2022-06-01 14:58:42 -0400

    init

---
+++ ContentBySeed.py
@@ -0,0 +1,46 @@
+###
+#Created by Derek Gooding | June 1, 2022
+#Shows a working proof of concept example for using random generation instead of hard coding color values.
+###
+import tkinter as tk
+import random
+from contextlib import suppress #allows for an inlined try/except that doesn't need an "except: pass". BaseException suppresses all exceptions
+
+#change this variable to increase or decrease seed variance
+RandomLimit = 10000 
+
+def RandomColor():
+    return "#" + ''.join([random.choice('0123456789ABCDEF') for _ in range(6)]) 
+      
+def RandomSeed():
+    input = inputbox.get('1.0', tk.END)
+    inputbox.delete('1.0', tk.END)
+    with suppress(BaseException): 
+        if  1 <= int(input) <= RandomLimit: 
+            return int(input)
+    random.seed()
+    return random.randint(1,RandomLimit)
+
+def Shuffle():
+    seed = RandomSeed()
+    random.seed(seed)   
+    label.config(text="Seed = " + str(seed))
+    [x.config(background=RandomColor()) for x in buttonArray]
+
+#Prepare visuals
+mainWindow = tk.Tk()
+mainWindow.geometry('100x250')
+inputbox = tk.Text(width=10, height=1)
+label = tk.Label()
+shuffleButton = tk.Button(text="Shuffle", command=Shuffle)
+buttonArray = [tk.Button(text=x, width=5, command=lambda n=x: print(n)) for x in range(6)]
+
+#Pack everything
+inputbox.pack(pady=2)
+label.pack()
+shuffleButton.pack(pady=1)
+for x in buttonArray:
+    x.pack(pady=1)
+
+Shuffle()
+mainWindow.mainloop()
